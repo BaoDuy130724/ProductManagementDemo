f3c182ea7d8f23beda51fb16aac26f370d4132d7
Le Tran Bao Duy <baoduy1372004@gmail.com>
2025-06-06 12:34:47 +0700

    Add project files.

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObjects;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IProductRepository _repository;
+        public ProductRepository(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public void DeleteProduct(Product product) => _repository.DeleteProduct(product);
+
+
+        public Product GetProductById(int id) => _repository.GetProductById(id);
+
+
+        public List<Product> GetProducts() => _repository.GetProducts();
+
+
+        public void SaveProduct(Product product) => _repository.SaveProduct(product);
+
+
+        public void UpdateProduct(Product product) => _repository.UpdateProduct(product);
+
+    }
+}
---
+++ Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObjects;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository iCategoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            iCategoryRepository = categoryRepository;
+        }
+        public List<Category> GetCategories()
+        {
+            return iCategoryRepository.GetCategories();
+        }
+    }
+}
