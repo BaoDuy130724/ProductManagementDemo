4c5461803ee5d2216cff4e99e091e69331fe2e1d
Le Tran Bao Duy <baoduy1372004@gmail.com>
2025-06-23 21:06:43 +0700

    fix lab1

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using BusinessObjects;
+using DataAccessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        public void DeleteProduct(Product product) => ProductDAO.DeleteProduct(product);
+
+        public Product GetProductById(int id) => _productDAO.GetProductById(id);
+
+        public List<Product> GetProducts()
+        {
+            
+        }
+
+        public void SaveProduct(Product p) => _productDAO.SaveProduct(p);
+
+        public void UpdateProduct(Product product) => _productDAO.UpdateProduct(product);
+    }
+}
